525e30c6b9e0519f09a277ac5aa14aa5adc78414
Thong Nguyen <thong.nguyen1820031956@hcmut.edu.vn>
2023-10-07 11:09:22 +0700

    doing-branch

---
+++ Report/Task1.md
@@ -0,0 +1,22 @@
+# MQTT and Firebase
+
+
+MQTT (Message Queuing Telemetry Transport) và Firebase là hai công nghệ phổ biến trong lĩnh vực truyền thông và lưu trữ dữ liệu. Dưới đây là một giới thiệu ngắn về cả hai:
+
+- **MQTT (Message Queuing Telemetry Transport):**
+
+    MQTT là một giao thức truyền thông nhị phân (binary protocol) được phát triển để gửi và nhận các thông điệp giữa các thiết bị hoặc ứng dụng một cách hiệu quả và nhẹ nhàng. MQTT thường được sử dụng trong các ứng dụng IoT (Internet of Things) và M2M (Machine-to-Machine) để truyền thông giữa các thiết bị IoT và máy chủ hoặc giữa các thiết bị IoT với nhau. MQTT hoạt động trên nguyên tắc publish/subscribe, trong đó các thiết bị có thể đăng ký để lắng nghe các chủ đề cụ thể và nhận thông điệp từ các chủ đề đó. Điều này làm cho MQTT trở thành một giao thức phổ biến trong việc thu thập và truyền tải dữ liệu từ các thiết bị IoT đến các hệ thống trung tâm
+
+- **Firebase:**
+
+    Firebase là một nền tảng phát triển ứng dụng di động và web được cung cấp bởi Google. Nó cung cấp một loạt các dịch vụ để phát triển, triển khai và quản lý ứng dụng di động và web dễ dàng và nhanh chóng. Firebase cung cấp nhiều tính năng, bao gồm cơ sở dữ liệu thời thực (Realtime Database) và Firestore (cơ sở dữ liệu đám mây), xác thực người dùng, lưu trữ tệp, thông báo đám mây, phân tích và nhiều tính năng khác. Firebase thường được sử dụng cho các ứng dụng di động và web cần tích hợp các tính năng như xác thực người dùng, lưu trữ dữ liệu và thông báo.
+
+# Read data from sensor then send to firebase
+
+1. Set up firebase : https://randomnerdtutorials.com/esp8266-nodemcu-firebase-realtime-database/
+2. Read value from sensor: In folder "Sourecode"
+3. Calculate the number of pins needed
+   - +3.3V, GND: 2 pins.
+   - DHT11: 1 pin.
+   - Stepper motor: 4 pins.
+   - Led: 1 pin.
---
+++ Sourecode/Task1/HandleSensor/src/main.cpp
@@ -0,0 +1,122 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com/esp8266-dht11dht22-temperature-and-humidity-web-server-with-arduino-ide/
+*********/
+
+// Import required libraries
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+
+// Replace with your network credentials
+const char* ssid = "thong";
+const char* password = "1234567890";
+
+#define DHTPIN 5     // Digital pin connected to the DHT sensor
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+JSONVar Readings;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;    // will store last time DHT was updated
+
+// Updates DHT readings every 10 seconds
+const long interval = 10000;  
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return String(t);
+  }
+  else if(var == "HUMIDITY"){
+    return String(h);
+  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  dht.begin();
+  LittleFS.begin();
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
+  }
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", String(), false, processor);
+  });
+  server.on("/css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/style.css", "text/css");
+  });
+  server.serveStatic("/", LittleFS, "/");
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(t).c_str());
+  });
+
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(h).c_str());
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop(){  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
+    if (isnan(newT)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      t = newT;
+      Readings["temperator"] = String(t);
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+    // if humidity read failed, don't change h value 
+    if (isnan(newH)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      h = newH;
+      Readings["humidity"] = String(h);
+    }
+    Serial.println(Readings);
+  }
+}
